56fe84eb0a24fbf05f8476d251db16075dca2a71
Serenity112 <skuratov.prodamgarazh@gmail.com>
2023-03-18 16:51:39 +0300

    Added lab5 template

---
+++ ComputerGraphics/Form5.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ComputerGraphics
+{
+    public partial class Form5 : Form
+    {
+        private readonly int _width;
+        private readonly int _height;
+
+        private readonly double _linesBrushWidth = 1;
+
+        private Graphics _graphics;
+
+        private Brush _blackBrush = new SolidBrush(Color.Black);
+        private Brush _blueBrush = new SolidBrush(Color.Blue);
+        private Brush _greenBrush = new SolidBrush(Color.Green);
+        private Brush _redBrush = new SolidBrush(Color.Red);
+        private Brush _purpleBrush = new SolidBrush(Color.Purple);
+
+        public Form5()
+        {
+            InitializeComponent();
+
+            _width = pictureBox1.Width;
+            _height = pictureBox1.Height;
+
+            _graphics = pictureBox1.CreateGraphics();
+            _graphics.TranslateTransform(_width / 2, _height / 2);
+            _graphics.ScaleTransform(1, -1);
+        }
+    }
+}
